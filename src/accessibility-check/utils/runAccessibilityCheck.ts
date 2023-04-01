23b540a6f499aa0f1cf9748458fe970afef36ed5
cradoe <noblewapinfo@gmail.com>
2023-04-01 19:20:43 +0100

    progress bar

--- src/accessibility-check/utils/runAccessibilityCheck.ts
+++ src/accessibility-check/utils/runAccessibilityCheck.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import { getSubPages } from "../../web-scraping";
 import { Issue } from "../interfaces";
 import { config } from "../../config";
+import cliProgress from "cli-progress";
 
 export async function runAccessibilityCheck(): Promise<Issue[]> {
   const configFile = await config();
@@ -17,10 +18,18 @@ export async function runAccessibilityCheck(): Promise<Issue[]> {
   }
 
   const pages: string[] = await getSubPages(rootUrl);
+  const progressBar = new cliProgress.SingleBar(
+    {},
+    cliProgress.Presets.shades_classic
+  );
 
   const MAX_CONCURRENT_REQUESTS: number = 5;
   const issues: Issue[] = [];
   const cache = new Map<string, Issue[]>();
+  let completedPages = 0;
+
+  // start the progress bar with a custom message
+  progressBar.start(100, 0, { text: "Running accessibility checks..." });
 
   // Process requests in batches of MAX_CONCURRENT_REQUESTS
   for (let i = 0; i < pages.length; i += MAX_CONCURRENT_REQUESTS) {
@@ -28,11 +37,11 @@ export async function runAccessibilityCheck(): Promise<Issue[]> {
     // @ts-ignore
     const pa11yPromises: Promise<Issue[] | undefined>[] = batch.map(
       async (page) => {
-        if (cache.has(page)) {
-          // Use cached result if available
-          return cache.get(page);
-        } else {
-          try {
+        try {
+          if (cache.has(page)) {
+            // Use cached result if available
+            return cache.get(page);
+          } else {
             const result = await pa11y(rootUrl + page);
             if (result.issues.length > 0) {
               result.issues.forEach((issue: any) => {
@@ -44,11 +53,17 @@ export async function runAccessibilityCheck(): Promise<Issue[]> {
               // Store empty array in cache to avoid making same request again
               cache.set(page, []);
             }
-          } catch (error) {
-            console.error(
-              `Failed to run accessibility check for ${page}: ${error}`
-            );
           }
+        } catch (error) {
+          console.error(
+            `Failed to run accessibility check for ${page}: ${error}`
+          );
+        } finally {
+          completedPages++;
+          const progress = Math.floor((completedPages / pages?.length) * 100);
+          progressBar.update(progress, {
+            text: `Running accessibility checks (${progress}% complete)...`,
+          });
         }
       }
     );
@@ -60,5 +75,8 @@ export async function runAccessibilityCheck(): Promise<Issue[]> {
       }
     }
   }
+
+  // stop the progress bar
+  progressBar.stop();
   return issues;
 }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { runAccessibilityCheck } from "./accessibility-check";
 (async () => {
   const issues = await runAccessibilityCheck();
 
-  console.clear();
   console.log(issues);
   console.log(`Total issues found: ${issues.length}`);
 })();
